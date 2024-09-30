13318b8f8ee59b402af871f609e0c369a8fb8bd3
Luis Herrera <luis1818.lh@gmail.com>
2024-09-30 16:57:48 -0500

    Primer commit de configuración

---
+++ src/main/java/com/luher/luher_pizzeria/LuherPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.luher.luher_pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuherPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuherPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luher/luher_pizzeria/LuherPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luher.luher_pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuherPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
