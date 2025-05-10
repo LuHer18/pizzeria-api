6fb88e325cce77356d22e592805a7494559713d6
Luis Herrera <luis1818.lh@gmail.com>
2025-05-10 13:18:43 -0500

    se crean los jwtfilters

--- src/main/java/com/luher/luher_pizzeria/web/config/JwtUtil.java
+++ src/main/java/com/luher/luher_pizzeria/web/config/JwtUtil.java
@@ -2,6 +2,7 @@ package com.luher.luher_pizzeria.web.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -10,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class JwtUtil {
 
-    private static String SECRET_KEY = "'Luher_prueb4";
+    private static String SECRET_KEY = "luher_prueb4";
 
     private static Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
 
@@ -21,5 +22,23 @@ public class JwtUtil {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
                 .sign(algorithm);
+    }
+
+    public boolean isValid(String jwt){
+        try {
+            JWT.require(algorithm)
+                    .build()
+                    .verify(jwt);
+            return true;
+        }catch (JWTVerificationException e){
+            return false;
+        }
+
+    }
+    public  String getUserName(String jwt){
+        return JWT.require(algorithm)
+                .build()
+                .verify(jwt)
+                .getSubject();
     }
 }
