9877b94431e72fe1448139f3845dbe15adda0c52
Luis Herrera <luis1818.lh@gmail.com>
2025-05-10 08:39:22 -0500

    se agrega configuracion para que no solo sea por rol

--- src/main/java/com/luher/luher_pizzeria/service/UserSecurityService.java
+++ src/main/java/com/luher/luher_pizzeria/service/UserSecurityService.java
@@ -4,12 +4,15 @@ import com.luher.luher_pizzeria.persistence.entity.UserEntity;
 import com.luher.luher_pizzeria.persistence.entity.UserRoleEntity;
 import com.luher.luher_pizzeria.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,25 +22,45 @@ public class UserSecurityService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Autowired
-    private UserSecurityService(UserRepository userRepository){
+    private UserSecurityService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-     UserEntity user = userRepository.findById(username)
-                 .orElseThrow(()-> new UsernameNotFoundException("User " + username + "no encontrado"));
+        UserEntity user = userRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + "no encontrado"));
         String[] roles = user.getRoles()
-                                .stream()
-                                .map(UserRoleEntity::getRole)
-                                .toArray(String[]::new);
+                .stream()
+                .map(UserRoleEntity::getRole)
+                .toArray(String[]::new);
 
         return User.builder()
                 .username(user.getUserName())
                 .password(user.getPassword())
-                .roles(roles)
+                .authorities(this.grantedAuthorityList(roles))
                 .accountLocked(user.getLocked())
                 .disabled(user.getDisable())
                 .build();
+    }
+
+    private String[] getAuthorities(String rol) {
+        if ("ADMIN".equals(rol) || "CUSTOMER".equals(rol)) {
+            return new String[]{"random_order"};
+        }
+
+        return new String[]{};
+    }
+
+    private List<GrantedAuthority> grantedAuthorityList(String[] roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>(roles.length);
+        for (String rol : roles) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + rol));
+            for (String authority:this.getAuthorities(rol)){
+                authorities.add(new SimpleGrantedAuthority(authority));
+            }
+        }
+
+        return authorities;
     }
 }
